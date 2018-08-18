ba6fc349558104f99c8dce44148704de8e3e88f8
Pravesh Gaire <praveshgaire.ioe@gmail.com>
2018-08-18 21:49:02 +0545

    Update koch.py

--- koch.py
+++ koch.py
@@ -6,24 +6,24 @@ from turtle import *
 #function to create koch snowflake or koch curve
 def snowflake(lengthSide, levels):
     if levels == 0:
-        print('Forward ' + str(lengthSide) + str(levels))
+#         print('Forward ' + str(lengthSide) + str(levels))
         forward(lengthSide)
-        input()
+#         input()
         return
     lengthSide /= 3.0
-    print('1st Snowflake function called recursively')
+#     print('1st Snowflake function called recursively')
     snowflake(lengthSide, levels-1)
     left(60)
-    print('Left 60 deg')
-    print('2nd Snowflake function called recursively')
+#     print('Left 60 deg')
+#     print('2nd Snowflake function called recursively')
     snowflake(lengthSide, levels-1)
     right(120)
-    print('Right 120')
-    print('3rd Snowflake function called recursively')
+#     print('Right 120')
+#     print('3rd Snowflake function called recursively')
     snowflake(lengthSide, levels-1)
     left(60)
-    print('Left 60')
-    print('4th Snowflake function called recursively')
+#     print('Left 60')
+#     print('4th Snowflake function called recursively')
     snowflake(lengthSide, levels-1)
 
 # main function
@@ -45,9 +45,9 @@ if __name__ == "__main__":
     # Pull the pen down – drawing when moving.
     pendown()
     for i in range(3):
-        print('snowflake function ' + str(i+1))
+#         print('snowflake function ' + str(i+1))
         snowflake(length, 3)
-        print('Right 120 deg')
+#         print('Right 120 deg')
         right(120)
 
     # To control the closing windows of the turtle
